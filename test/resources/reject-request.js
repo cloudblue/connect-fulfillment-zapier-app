efe0fb6767d4bc1c39afb9f2d48e9091a5bbb33b
Martin Constante <martin.constante@ingrammicro.com>
2019-12-03 15:25:35 +0100

    LITE-11071 Create call in App zapier that allow reject a request

---
+++ test/resources/reject-request.js
@@ -0,0 +1,46 @@
+/**
+ * This file is part of the Ingram Micro Cloud Blue Connect SDK.
+ *
+ * @copyright (c) 2019. Ingram Micro. All Rights Reserved.
+ */
+
+/* globals describe it */
+const should = require('should');
+const getConnectClient = require('../../lib/utils').getConnectClient;
+const HttpError = require('connect-javascript-sdk').HttpError;
+const sinon = require('sinon');
+const zapier = require('zapier-platform-core');
+
+// Use this to make test calls into your app:
+const App = require('../../index');
+const appTester = zapier.createAppTester(App);
+zapier.tools.env.inject();
+
+describe('Connect Zapier App', () => {
+  let sandbox;
+  before(() => { sandbox = sinon.createSandbox(); });
+  afterEach(done => { sandbox.restore(); done(); });
+  it('should return a payload of the rejected request', done => {
+    const bundle = {
+      authData: {
+        api_key: process.env.CONNECT_API_KEY,
+        endpoint: process.env.CONNECT_ENDPOINT
+      },
+      inputData: {
+        id: 'PR-5426-PR-5426-9883-2189-001',
+        reason: 'Not available'
+      }
+    };
+
+    // Mock the sdk function to return this response 
+    sandbox.stub(getConnectClient({request: null}, bundle).requests, 'rejectRequest').returns(require('../samples/reject-request'));
+    // Call to zapier function to test
+    appTester(App.creates.rejectRequest.operation.perform, bundle)
+      .then(results => {
+        results.should.be.an.Object();
+        results.status.should.be.eql('failed');
+        done();
+      })
+      .catch(done);
+  });
+});
