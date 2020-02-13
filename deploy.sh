f845b2334f3166baa2d76cd864dbfca056ce81f3
Francesco Faraone <francesco.faraone@ingrammicro.com>
2020-02-13 23:02:05 +0100

    install silent

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-WHOAMI=$(whoami)
 
 CONNECT_ZAPIER_VERSION=$(node -p "require('./package.json').version")
 
 echo "Deploy Connect Fulfillment Zapier App version $CONNECT_ZAPIER_VERSION to zapier...."
-
-echo "Validate version...."
-
-# zapier validate
 
 echo "Generate rc files..."
 
@@ -18,12 +13,6 @@ cat >$HOME/.zapierrc<<EOF
   "deployKey": "$CONNECT_ZAPIER_DEPLOY_KEY"
 }
 EOF
-
-echo "Home is $HOME and user is $WHOAMI"
-
-tail -2 $HOME/.zapierrc |cut -b 1-20
-
-echo "***************"
 
 cat >.zapierapprc<<EOF
 {
