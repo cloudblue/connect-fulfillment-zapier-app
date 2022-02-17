21c489041914ad8c82f4f0519ed11b3cad68c64b
Martin Constante <martin.constante@ingrammicro.com>
2022-02-17 16:26:01 +0100

    LITE-22276-Add-default-fileter-inGET-New_Cases

--- tests/specs/fields/input/searchCases.spec.js
+++ tests/specs/fields/input/searchCases.spec.js
@@ -26,19 +26,6 @@ describe('search cases', () => {
     const fields = await fn(null, bundle);
     expect(fields).toEqual([
       { 
-        key: 'state',
-        required: false,
-        label: 'Case Status',
-        helpText: 'Using this filter only cases matching the requested status will be available for the Zap.',
-        list: false,
-        choices: {
-                'closed':'Closed',
-                'inquiring':'Inquiring',
-                'pending':'Pending',
-                'resolved':'Resolved',
-        }
-      },
-      { 
         key: 'priority',
         label: 'Using this filter only cases matching the requested Priority will be available for the Zap.',
         list: false ,
